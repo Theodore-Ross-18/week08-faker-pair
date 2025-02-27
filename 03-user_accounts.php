2b29907a967aeeaf57f16717b3d0c38b90e0bd92
Theodore-Ross-18 <thedoreross18@gmail.com>
2025-02-27 09:36:30 +0800

    PHP HTML

--- 03-user_accounts.php
+++ 03-user_accounts.php
@@ -1,1 +1,19 @@
+<?php
 
+require './vendor/autoload.php';
+
+$faker = Faker\Factory::create();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Accounts</title>
+</head>
+<body>
+    
+</body>
+</html>
