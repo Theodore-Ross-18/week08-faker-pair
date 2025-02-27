9a36de2edc9bd0a0e1e255120158a2cb004df25f
pavseh <pavsehfx@gmail.com>
2025-02-27 09:31:40 +0800

    Bootstrap CSS

--- 02-books.php
+++ 02-books.php
@@ -15,12 +15,15 @@ $genres = ['Fiction','Mystery','Science Fiction','Fantasy',
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Online Library</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body>
-    <div>
-        <table>
+    <div class="container mt-5">
+    <h2 class="fw-bold">Online Library Book Records</h2>
+        <table class="table table-bordered table-striped">
             <thead>
-                <tr>
+                <tr class="fw-bold">
                     <th>Title</th>
                     <th>Author</th>
                     <th>Genre</th>
