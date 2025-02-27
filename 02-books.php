ed8d2a40a107c5d393e33349ca4cbfc6f512fedd
pavseh <pavsehfx@gmail.com>
2025-02-27 09:12:35 +0800

    Faker PHP + Genres

---
+++ 02-books.php
@@ -0,0 +1,10 @@
+<?php
+
+require './vendor/autoload.php';
+$faker = Faker\Factory::create();
+
+$genre = ['Fiction','Mystery','Science Fiction','Fantasy',
+'Romance', 'Thriller', 'Historical', 'Horror'];
+
+
+?>
