0425d498d8c3a47b2e88e04d38d8eb758d46e054
pavseh <pavsehfx@gmail.com>
2025-02-27 09:19:10 +0800

    HTML and Tables

--- 02-books.php
+++ 02-books.php
@@ -3,8 +3,36 @@
 require './vendor/autoload.php';
 $faker = Faker\Factory::create();
 
-$genre = ['Fiction','Mystery','Science Fiction','Fantasy',
+$genres = ['Fiction','Mystery','Science Fiction','Fantasy',
 'Romance', 'Thriller', 'Historical', 'Horror'];
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Library</title>
+</head>
+<body>
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Author</th>
+                    <th>Genre</th>
+                    <th>Publication Year</th>
+                    <th>ISBN</th>
+                    <th>Summary</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+    </div>
+    
+</body>
+</html>
