5e1bb8a4a07e8c874dc01c91ccada2fc0dcdb21f
Theodore-Ross-18 <thedoreross18@gmail.com>
2025-02-27 08:38:01 +0800

    THEAD tr th

--- 01-user_profiles.php
+++ 01-user_profiles.php
@@ -18,7 +18,14 @@ $faker = Faker\Factory::create('en_PH');
         <h2>User Profiles</h2>
         <table class="table table-bordered table-striped">
             <thead>
-                
+                <tr>
+                    <th>Full Name</th>
+                    <th>Email Address</th>
+                    <th>Phone Number</th>
+                    <th>Complete Address</th>
+                    <th>Birthdate</th>
+                    <th>Job Title</th>
+                </tr>
             </thead>
             <tbody>
                 
